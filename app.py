7d95462721bd4dc1c8b253c2c557c8c8bf66c18c
sneijderbk147VS <sneijderbk7@gmail.com>
2026-09-24 09:19:31 -0500

    Forzando carga de plantilla index.html

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
+import os
 from flask import Flask, render_template
 
-app = Flask(__name__)
+app = Flask(__name__, template_folder='templates', static_folder='static')
 
 @app.route("/")
 def home():
